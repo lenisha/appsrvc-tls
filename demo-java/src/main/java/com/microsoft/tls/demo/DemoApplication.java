0bbe58c5564fe9900d1617418c604542066521e0
Elena Neroslavskaya <lenisha@gmail.com>
2018-12-17 20:34:22 -0500

    init

---
+++ demo-java/src/main/java/com/microsoft/tls/demo/DemoApplication.java
@@ -0,0 +1,49 @@
+package com.microsoft.tls.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication extends SpringBootServletInitializer {
+
+
+	RestTemplate restTemplate = new RestTemplate();
+
+	@RequestMapping("/hello")
+	public String home() {
+		return "Hello world";
+	}
+
+
+	// https://www.oracle.com/technetwork/articles/javase/security-137537.html
+	//https://blogs.msdn.microsoft.com/appserviceteam/2017/06/27/installing-public-certificates-in-app-service/
+	// set-location cert:\CurrentUser\My
+	// get-childitem
+	@RequestMapping("/callhello")
+	public String callHello() {
+		//System.setProperty("javax.net.ssl.trustStore","NUL");
+		//System.setProperty("javax.net.ssl.trustStoreType","Windows-MY");
+		ResponseEntity<String> response = restTemplate.exchange("https://consul.altostratus.me/demo/hello",HttpMethod.GET,null,String.class);
+		return "Invoked and received payload: " + response.toString();
+	}
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+}
+
