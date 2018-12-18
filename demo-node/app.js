e0a92d38df29b7f4e4c870d7e4e680ad84ef73cf
Elena Neroslavskaya <lenisha@gmail.com>
2018-12-18 01:43:54 -0500

    add http invoke node

--- demo-node/app.js
+++ demo-node/app.js
@@ -1,7 +1,18 @@
 var express = require('express');
+var request = require("request");
+
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!  Use Azure!');
+  
+  request('https://consul.altostratus.me/demo/hello', (err, resp, body) => {
+     if (err) { 
+       res.send(err);
+       return console.log(err); 
+     }
+     console.log(body.url);
+     console.log(body.explanation);
+     res.send('Hello invoked and received:' + body);
+  });
 });
 var port = process.env.PORT || 8000;
 app.listen(port, function () {
